4a3d1e5ab204fb6c2769cf3d0bb00313c38dd0b6
Yann Vaillant <yann@vaillant.im>
2014-11-23 19:22:03 +0100

    basic rss model

---
+++ desktop-client/src/com/vayan/rss/rssFeed.java
@@ -0,0 +1,12 @@
+package com.vayan.rss;
+
+import java.util.List;
+
+/**
+ * Created by yann on 23/11/2014.
+ */
+public class rssFeed {
+    private String name;
+    private String url;
+    private List<rssItem> rssItems;
+}
---
+++ desktop-client/src/com/vayan/rss/rssItem.java
@@ -0,0 +1,9 @@
+package com.vayan.rss;
+
+/**
+ * Created by yann on 23/11/2014.
+ */
+public class rssItem {
+    private String title;
+    private String content;
+}
